685bd0fdc1b82edd5c223d811542bffd67859483
niansong1996 <niansong1996@163.com>
2018-09-26 20:21:57 -0400

    add the embedding file

---
+++ embed.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+
+'''
+    functions to generate embedding for each word and sentences
+'''
+
+from typing import List
+
+def corpus_to_embedding(corpus: List[List[str]]) -> Tensor:
+
+    raise NotImplementedError()
+
+
+def embedding_to_corpus(embedding: Tensor) -> List[List[str]]:
+
+    raise NotImplementedError()
