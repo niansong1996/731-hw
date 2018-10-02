02edd0c59e2bfa54a524855be28b862c653d29bc
niansong1996 <niansong1996@163.com>
2018-10-02 01:15:46 -0400

    fix conflict

--- nmt.py
+++ nmt.py
@@ -87,14 +87,12 @@ class NMT(nn.Module):
         self.encoder_embed = nn.Embedding(src_vocab_size, embed_size, padding_idx=0)
         self.encoder_lstm = nn.LSTM(embed_size, hidden_size, dropout=self.dropout_rate, bidirectional=True)
 
-<<<<<<< HEAD
         self.decoder_embed = nn.Embedding(self.tgt_vocab_size, embed_size, padding_idx=0)
-        self.decoder_state_transform = nn.Linear(hidden_size * 2, hidden_size * 2)
+
         self.decoder_lstm = nn.LSTM(embed_size, 2 * hidden_size)
-=======
         self.decoder_embed = nn.Embedding(self.tgt_vocab_size, embed_size)
+        self.decoder_state_transform = nn.Linear(hidden_size * 2, hidden_size * 2)
         self.decoder_lstm = nn.LSTM(2 * hidden_size + embed_size, 2 * hidden_size, dropout=self.dropout_rate)
->>>>>>> e8f8abf0b2473091cf5ce7bd52450a5f54a8364d
         self.decoder_out = nn.Linear(hidden_size, self.tgt_vocab_size)
         # W_a for attention
         self.decoder_W_a = nn.Linear(hidden_size * 2, hidden_size * 2, bias=False)
