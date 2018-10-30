bb7ac4ffcf4a83bba5a340a231f4c3aa8e171908
niansong1996 <niansong1996@163.com>
2018-10-30 15:45:07 -0400

    revise train

--- multilingual/train.sh
+++ multilingual/train.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-vocab="data/vocab.bin"
-
 work_dir="work_dir"
 name_prefix="multi-lang"
 model_name=${name_prefix}"-model.bin"
 decode=${name_prefix}"-result.txt"
-test_tgt="data/test.en-az.en.txt"
 mkdir -p ${work_dir}
 echo save results to ${work_dir}
 
@@ -34,14 +31,9 @@ python nmt.py \
     --patience 3
 # 2>${work_dir}/err.log
 
-python nmt.py \
-    decode \
-    --cuda \
-    --beam-size 5 \
-    --max-decoding-time-step 100 \
-    ${work_dir}/${model_name} \
-    az \
-    en \
-    ${work_dir}/${decode}
-
-perl multi-bleu.perl ${test_tgt} < ${work_dir}/${decode}
+for lang in {az, be, gl, tr, ru, pt}
+do
+python nmt.py decode --cuda --beam-size 5 --max-decoding-time-step 100 \
+    ${work_dir}/${model_name} $lang en ${work_dir}/decode-$lang.txt
+perl multi-bleu.perl data/test.$lang-en.en.txt < ${work_dir}/decode-$lang.txt
+done
