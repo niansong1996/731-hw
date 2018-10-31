bdfe10ca975adff947bf6046340d95fb624a0a53
mao <maocf1993@gmail.com>
2018-10-31 05:21:58 +0000

    BLEU 21.61

--- multilingual/train.sh
+++ multilingual/train.sh
@@ -7,33 +7,8 @@ decode=${name_prefix}"-result.txt"
 mkdir -p ${work_dir}
 echo save results to ${work_dir}
 
-python nmt.py \
-    train \
-    --langs 'de-en'\
-    --lang-embed-size 8\
-    --cuda \
-    --vocab-size 20000 \
-    --save-to ${work_dir}/${model_name} \
-    --save-opt ${work_dir}/optimizer.bin \
-    --valid-niter 1000 \
-    --lr 0.001 \
-    --log-every 50 \
-    --batch-size 32 \
-    --hidden-size 256 \
-    --low-rank 3 \
-    --num-layers 1 \
-    --max-epoch 100 \
-    --embed-size 256 \
-    --uniform-init 0.1 \
-    --dropout 0.2 \
-    --clip-grad 5.0 \
-    --lr-decay 0.5 \
-    --patience 3
 # 2>${work_dir}/err.log
 
-for lang in {de}
-do
 python nmt.py decode --cuda --beam-size 5 --max-decoding-time-step 100 \
-    ${work_dir}/${model_name} $lang en ${work_dir}/decode-$lang.txt
-perl multi-bleu.perl data/test.$lang-en.en.txt < ${work_dir}/decode-$lang.txt
-done
+    ${work_dir}/${model_name} de en ${work_dir}/decode-de.txt
+perl multi-bleu.perl data/test.de-en.en.txt < ${work_dir}/decode-de.txt
