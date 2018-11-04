6795833a6cf9340c9739ed12e5fa8cd39d9fe38d
mao <maocf1993@gmail.com>
2018-11-03 22:36:39 -0400

    add genereate dict

---
+++ clean_file.py
@@ -0,0 +1,26 @@
+def clean(src_file, tgt_file, src_output, tgt_output):
+    src_lines = []
+    for line in open(src_file, encoding="utf-8"):
+        src_lines.append(line)
+    tgt_lines = []
+    for line in open(tgt_file, encoding="utf-8"):
+        tgt_lines.append(line)
+    src_result = []
+    tgt_result = []
+    for src, tgt in zip(src_lines, tgt_lines):
+        len_ratio = len(src.split(" ")) / len(tgt.split(" "))
+        if 0.8 < len_ratio < 1.2:
+            src_result.append(src)
+            tgt_result.append(tgt)
+    with open(src_output, 'w') as f:
+        for line in src_result:
+            f.write(line)
+    with open(tgt_output, 'w') as f:
+        for line in tgt_result:
+            f.write(line)
+    print("reduce %d lines to %d"%(len(src_lines), len(src_result)))
+
+
+if __name__ == "__main__":
+    clean("multilingual/data/train.ru-be.ru.txt", "multilingual/data/train.ru-be.be.txt",
+          "cleaned-ru-be.ru", "cleaned-ru-be.be")
---
+++ translate_with_dict.py
@@ -0,0 +1,75 @@
+def lookup_dict(w, d, trans_cnt):
+    if w in d:
+        trans_cnt[0] = trans_cnt[0] + 1
+        # return '>>>[{}<-{}]<<<'.format(d[w], w)
+        return d[w]
+    return w
+
+
+def translate(input_file, output_file, d):
+    line_num = 1
+    trans_line = []
+    with open(output_file, 'w') as f:
+        for line in open(input_file, encoding="utf-8"):
+            sent = line.strip().split(' ')
+            trans_cnt = [0]
+            translated_words = map(lambda w:  lookup_dict(w, d, trans_cnt), sent)
+            trans_sent = ' '.join(translated_words)
+            if '[' in trans_sent:
+                trans_line.append(line_num)
+            line_num += 1
+            f.write(trans_sent + '\n')
+    return trans_line
+
+
+def get_vocab(file):
+    vocab = {}
+    for line in open(file, encoding="utf-8"):
+        for w in line.strip().split(' '):
+            if w in vocab:
+                vocab[w] += 1
+            else:
+                vocab[w] = 0
+    return vocab
+
+
+def read_dict(file_path, src_vocab, tgt_vocab):
+    d = {}
+    line_count = 0
+    ommitted_cnt = 0
+    for line in open(file_path, encoding="utf-8"):
+        entry = line.strip().split(' ')
+        line_count += 1
+        src = entry[0]
+        tgt = entry[1]
+        score = float(entry[2])
+        if src == tgt:
+            ommitted_cnt += 1
+            continue
+        if src not in src_vocab or src_vocab[src] < 3:
+            continue
+        if tgt not in tgt_vocab or tgt_vocab[tgt] < 3:
+            continue
+        if score < 0.6:
+            break
+        d[src] = tgt
+    print("Got dict of size %d; omitted %d words with the same spelling"%(len(d), ommitted_cnt))
+    return d
+
+if __name__ == '__main__':
+    # src_vocab = get_vocab("cleaned_tr-az.tr")
+    # tgt_vocab = get_vocab("cleaned_tr-az.az")
+    # d = read_dict('lex.tr2az.sorted', src_vocab, tgt_vocab)
+    # lines = translate('multilingual/data/test.tr-az.tr.txt', 'test.result.tr-az.az.txt', d)
+    # print(lines)
+
+    # src_vocab = get_vocab("cleaned-pt-gl.pt")
+    # tgt_vocab = get_vocab("cleaned-pt-gl.gl")
+    # d = read_dict('lex.pt2gl.sorted', src_vocab, tgt_vocab)
+    # lines = translate('multilingual/data/test.pt-gl.pt.txt', 'test.result.pt-gl.gl.txt', d)
+    # print(lines)
+    src_vocab = get_vocab("cleaned-ru-be.ru")
+    tgt_vocab = get_vocab("cleaned-ru-be.be")
+    d = read_dict('lex.ru2be.sorted', src_vocab, tgt_vocab)
+    lines = translate('multilingual/data/test.ru-be.ru.txt', 'test.result.ru-be.be.txt', d)
+    print(lines)
